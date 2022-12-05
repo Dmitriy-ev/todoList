d6195ebceb1cea17c100e3dd5857e5c6d34e2ec8
Dmitrii Evmenov <dmitrii.evmenov@btsdigital.kz>
2022-12-05 14:41:52 +0300

    initial commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.7.20"
     application
+    jacoco
+    id("io.qameta.allure") version "2.11.2"
 }
 
 group = "org.example"
@@ -15,10 +17,27 @@ repositories {
 dependencies {
     testImplementation(kotlin("test"))
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.7.20")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+    testImplementation("org.testng:testng:7.1.0")
 }
 
-tasks.test {
+tasks.withType<Test> {
     useJUnitPlatform()
+    finalizedBy("jacocoTestReport", "allureReport")
+}
+
+allure {
+    report {
+        version.set("2.20.0")
+    }
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required.set(false)
+        csv.required.set(false)
+        html.outputLocation.dir("${buildDir}/reports/coverage")
+    }
 }
 
 tasks.withType<KotlinCompile> {
---
+++ src/test/kotlin/test/data/TasksRepositoryMemoryTest.kt
@@ -0,0 +1,59 @@
+package test.data
+
+import data.Priority
+import data.Task
+import data.TasksRepositoryMemory
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+internal class TasksRepositoryMemoryTest {
+
+
+    private lateinit var repository: TasksRepositoryMemory
+
+    @BeforeEach
+    fun init() {
+        repository = TasksRepositoryMemory()
+        repository.addTask(Task(1, "Task1", Priority.HIGH, true))
+        repository.addTask(Task(2, "Task2", Priority.LOW))
+        repository.addTask(Task(3, "Task3", Priority.MEDIUM, true))
+        repository.addTask(Task(4, "Task4", Priority.HIGH))
+        repository.addTask(Task(5, "Task5", Priority.MEDIUM))
+
+    }
+
+    @Test
+    fun getTask() {
+        assertEquals(5, repository.getTasks().size)
+    }
+
+    @Test
+    fun addTask() {
+        val newTask = Task(6, "Task6", Priority.LOW)
+        repository.addTask(newTask)
+        assertEquals(newTask, repository.tasks.first { it.id == 6 })
+    }
+
+    @Test
+    fun deleteTask() {
+        assertEquals(5, repository.getTasks().size)
+        repository.deleteTask(3)
+        assertEquals(4, repository.getTasks().size)
+    }
+
+    @Test
+    fun completeTask() {
+        assertEquals(3, repository.getTasks(false).size)
+        repository.completeTask(2)
+        assertEquals(2, repository.getTasks(false).size)
+    }
+
+    @Test
+    fun uncompleteTask() {
+        assertEquals(3, repository.getTasks(false).size)
+        repository.uncompleteTask(1)
+        assertEquals(4, repository.getTasks(false).size)
+
+    }
+}
