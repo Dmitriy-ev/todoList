ba124cbd9462a634cb09d0866c5235a717020a18
Dmitrii Evmenov <dmitrii.evmenov@btsdigital.kz>
2023-01-12 14:40:38 +0300

    fix

--- src/test/kotlin/test/data/TasksRepositoryMemoryTest.kt
+++ src/test/kotlin/test/data/TasksRepositoryMemoryTest.kt
@@ -2,37 +2,44 @@ package test.data
 
 import data.Priority
 import data.Task
+import data.TasksRepository
 import data.TasksRepositoryMemory
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import kotlin.test.BeforeTest
 
 internal class TasksRepositoryMemoryTest {
 
 
-    private lateinit var repository: TasksRepositoryMemory
+    private lateinit var repository: TasksRepository
 
-    @BeforeEach
+    @BeforeTest
     fun init() {
-        repository = TasksRepositoryMemory()
-        repository.addTask(Task(1, "Task1", Priority.HIGH, true))
-        repository.addTask(Task(2, "Task2", Priority.LOW))
-        repository.addTask(Task(3, "Task3", Priority.MEDIUM, true))
-        repository.addTask(Task(4, "Task4", Priority.HIGH))
-        repository.addTask(Task(5, "Task5", Priority.MEDIUM))
-
+        repository = TasksRepositoryMemory().apply {
+            addTask(Task(1, "Task1", Priority.HIGH, true))
+            addTask(Task(2, "Task2", Priority.LOW))
+            addTask(Task(3, "Task3", Priority.MEDIUM, true))
+            addTask(Task(4, "Task4", Priority.HIGH))
+            addTask(Task(5, "Task5", Priority.MEDIUM))
+        }
     }
 
     @Test
-    fun getTask() {
+    fun getAllTask() {
         assertEquals(5, repository.getTasks().size)
+    }
+
+    @Test
+    fun getTaskWithFalseParameter() {
+        assertEquals(3, repository.getTasks(false).size)
     }
 
     @Test
     fun addTask() {
         val newTask = Task(6, "Task6", Priority.LOW)
         repository.addTask(newTask)
-        assertEquals(newTask, repository.tasks.first { it.id == 6 })
+        assertEquals(newTask, repository.getTasks().first { it.id == 6 })
     }
 
     @Test
